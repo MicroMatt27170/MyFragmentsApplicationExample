82c18eaad9bdaa0af6aea86b84afbb07a1021637
Matthew Montes de Oca <mmmatthew1999@outlook.com>
2018-08-23 20:49:24 -0500

    Initial commit

---
+++ app/src/main/java/com/matthew/micromatt/myfragmentsapplication/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.matthew.micromatt.myfragmentsapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
